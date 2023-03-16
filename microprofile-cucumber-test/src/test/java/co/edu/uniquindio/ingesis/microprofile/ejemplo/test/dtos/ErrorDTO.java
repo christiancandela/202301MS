cf831488c9da76c97361dac3482cc58ebf9c32e3
cacandela <christiancandela@gmail.com>
2023-03-16 18:06:09 -0500

    fea: se modifica DTO

--- microprofile-cucumber-test/src/test/java/co/edu/uniquindio/ingesis/microprofile/ejemplo/test/dtos/ErrorDTO.java
+++ microprofile-cucumber-test/src/test/java/co/edu/uniquindio/ingesis/microprofile/ejemplo/test/dtos/ErrorDTO.java
@@ -1,15 +1,12 @@
 package co.edu.uniquindio.ingesis.microprofile.ejemplo.test.dtos;
 
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
 import java.beans.ConstructorProperties;
 
 @Getter
+@RequiredArgsConstructor(onConstructor_= @ConstructorProperties({"error"}))
 public class ErrorDTO {
     private final String error;
-
-    @ConstructorProperties({"error"})
-    public ErrorDTO(String error) {
-        this.error = error;
-    }
 }
--- microprofile-cucumber-test/src/test/java/co/edu/uniquindio/ingesis/microprofile/ejemplo/test/dtos/TokenDTO.java
+++ microprofile-cucumber-test/src/test/java/co/edu/uniquindio/ingesis/microprofile/ejemplo/test/dtos/TokenDTO.java
@@ -2,21 +2,15 @@ package co.edu.uniquindio.ingesis.microprofile.ejemplo.test.dtos;
 
 import lombok.Builder;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
 import java.beans.ConstructorProperties;
 import java.time.LocalDateTime;
 
 @Getter
-@Builder
+@RequiredArgsConstructor(onConstructor_= @ConstructorProperties({"token", "vigencia","usuario"}))
 public class TokenDTO {
     private final String token;
     private final LocalDateTime vigencia;
     private final String usuario;
-
-    @ConstructorProperties({"token", "vigencia","usuario"})
-    public TokenDTO(String token, LocalDateTime vigencia, String usuario) {
-        this.token = token;
-        this.vigencia = vigencia;
-        this.usuario = usuario;
-    }
 }
